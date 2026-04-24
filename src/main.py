78e6a43bed2bca0d09fdbfd3504272d1fd6965c4
JCCG Code <jccuadradogracia@gmail.com>
2026-04-24 12:45:06 +0200

    extraexercises done

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from models import CompletionRequest, CompletionResponse
+from models import ModelConfig, CompletionRequest, CompletionResponse
 
 app = FastAPI(title="Unified LLM Gateway")
 
@@ -7,7 +7,9 @@ app = FastAPI(title="Unified LLM Gateway")
 @app.get("/health")
 async def health() -> CompletionResponse:
     return CompletionResponse(
-        model="MiniMax-M2.7",
+        model=ModelConfig(
+            name="MiniMax-M2.7", cost_input_token=0.05, cost_output_token=0.1
+        ),
         content="Hola que tal",
         input_tokens=10,
         output_tokens=10,
@@ -17,11 +19,16 @@ async def health() -> CompletionResponse:
 
 @app.post("/complete")
 async def complete(req: CompletionRequest) -> CompletionResponse:
+    # Requested model
+    model = ModelConfig(name=req.model, cost_input_token=0.05, cost_output_token=0.1)
+    input_tokens = 10
+    output_tokens = 10
+    # Return statement
     return CompletionResponse(
-        model=req.model,
+        model=model,
         content=f"Respuesta simulada para: {req.prompt}",
-        # Contar tokens de req.prompt
-        input_tokens=10,
-        output_tokens=10,
-        cost_usd=1.1,
+        input_tokens=input_tokens,
+        output_tokens=output_tokens,
+        cost_usd=model.cost_input_token * input_tokens
+        + model.cost_output_token * output_tokens,
     )
--- src/models.py
+++ src/models.py
@@ -1,4 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, AfterValidator, Field
+from datetime import datetime
+from typing import Annotated
+
+
+# Checks a valid temperature
+def valid_temperature(value: float) -> float:
+    if value < 0 or value > 2:
+        raise ValueError("No es una temperatura valida")
+    return value
+
+
+# Model configuration interface
+class ModelConfig(BaseModel):
+    name: str
+    cost_input_token: float
+    cost_output_token: float
 
 
 # Completion request /complete endpoint
@@ -6,14 +22,15 @@ class CompletionRequest(BaseModel):
     model: str
     prompt: str
     max_tokens: int = 1000
-    temperature: float = 0.7
+    temperature: Annotated[float, AfterValidator(valid_temperature)]
     stream: bool = False
 
 
 # Completion response /complete endpoint
 class CompletionResponse(BaseModel):
-    model: str
+    model: ModelConfig
     content: str
     input_tokens: int
     output_tokens: int
     cost_usd: float
+    created_at: datetime = Field(default_factory=datetime.now)
